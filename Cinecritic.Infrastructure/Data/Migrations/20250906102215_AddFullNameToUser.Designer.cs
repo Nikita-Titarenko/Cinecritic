6df17e0f0c4e2155d9f62e5e80a5b9a86fb4967c
Mykyta Titarenko <mykyta.titarenko@nure.ua>
2025-09-07 14:30:09 +0300

    Implement ApplicationUser

--- Cinecritic.Infrastructure/Data/Migrations/20250906102215_AddFullNameToUser.Designer.cs
+++ Cinecritic.Infrastructure/Data/Migrations/20250906102215_AddFullNameToUser.Designer.cs
@@ -4,6 +4,7 @@ using Cinecritic.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cinecritic.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250906102215_AddFullNameToUser")]
+    partial class AddFullNameToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -49,7 +52,7 @@ namespace Cinecritic.Migrations
 
                     b.HasIndex("MovieTypeId");
 
-                    b.ToTable("Movies", (string)null);
+                    b.ToTable("Movies");
                 });
 
             modelBuilder.Entity("Cinecritic.Domain.Models.MovieType", b =>
@@ -66,7 +69,7 @@ namespace Cinecritic.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("MovieTypes", (string)null);
+                    b.ToTable("MovieTypes");
 
                     b.HasData(
                         new
