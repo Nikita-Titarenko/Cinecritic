6d0d192813ca05fc1d0736de42e7fe28464b5e53
Mykyta Titarenko <mykyta.titarenko@nure.ua>
2025-09-13 13:56:41 +0300

    Rename property from WatchList to WatchLists

--- Cinecritic.Domain/Models/Movie.cs
+++ Cinecritic.Domain/Models/Movie.cs
@@ -13,6 +13,6 @@ namespace Cinecritic.Domain.Models
         public MovieType MovieType { get; set; } = default!;
         public int MovieTypeId {  get; set; }
         public IEnumerable<MovieUser> MovieUsers { get; set; } = new List<MovieUser>();
-        public IEnumerable<WatchList> WatchLists { get; set; } = new List<WatchList>();
+        public IEnumerable<WatchList> WatchList { get; set; } = new List<WatchList>();
     }
 }
